1fe0975ce296fe9596d0d75d19bbed45d421643f
Dan G <daniel.w.gray1994@gmail.com>
2026-02-15 14:33:58 -0500

    added styling to EntryCard

--- src/components/entries/journal/EntryCard.tsx
+++ src/components/entries/journal/EntryCard.tsx
@@ -7,8 +7,10 @@ type EntryCardProps = {
 
 export default function EntryCard({ entry }: EntryCardProps) {
   return (
-    <Surface className="w-full" hover>
-      EntryCard
+    <Surface className="w-full h-22 px-3 py-2" hover>
+      <li>
+        <h3>{`${entry.userId}'s Entry - ${entry.createdAt}`}</h3>
+      </li>
     </Surface>
   );
 }
