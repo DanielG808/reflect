d2ef93c8a9205480300f884214132656190626d8
Dan G <daniel.w.gray1994@gmail.com>
2026-02-15 14:03:53 -0500

    added EntriesList to the EntriesPage

--- app/(app)/entries/page.tsx
+++ app/(app)/entries/page.tsx
@@ -1,3 +1,15 @@
+import EntriesList from "@/src/components/entries/journal/EntriesList";
+import MainContainer from "@/src/components/layout/MainContainer";
+import H1 from "@/src/components/ui/H1";
+
 export default function EntriesPage() {
-  return <div>EntriesPage</div>;
+  return (
+    <MainContainer
+      fullBleed
+      className="h-full flex flex-col min-h-0 overflow-hidden"
+    >
+      <H1 className="text-lg">My Journal:</H1>
+      <EntriesList />
+    </MainContainer>
+  );
 }
---
+++ src/components/entries/journal/EntriesList.tsx
@@ -0,0 +1,5 @@
+export default function EntriesList() {
+  return (
+    <ul className="flex flex-col items-center w-1/3 pt-10">EntriesList</ul>
+  );
+}
