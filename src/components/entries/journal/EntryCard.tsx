7e5c74df52f794bb6490198d170a6567c8905d1b
Dan G <daniel.w.gray1994@gmail.com>
2026-02-15 15:44:09 -0500

    made EntryCard a link to /entry/[id]

--- src/components/entries/journal/EntryCard.tsx
+++ src/components/entries/journal/EntryCard.tsx
@@ -2,6 +2,7 @@ import { EntryDTO } from "@/src/lib/entries/types";
 import Surface from "../../ui/Surface";
 import { formatEntryDate } from "@/src/lib/utils/time";
 import DeleteEntryButton from "./DeleteEntryButton";
+import Link from "next/link";
 
 type EntryCardProps = {
   username: string;
@@ -12,11 +13,13 @@ export default function EntryCard({ username, entry }: EntryCardProps) {
   const formattedDate = formatEntryDate(entry.createdAt);
 
   return (
-    <Surface className="group w-full h-22 px-3 py-2" hover>
-      <div className="flex justify-between text-center">
-        <h3 className="font-semibold">{`✧ ${username}'s entry · ${formattedDate}`}</h3>
-        <DeleteEntryButton entryId={entry.id} />
-      </div>
-    </Surface>
+    <Link href={`/entry/${entry.id}`}>
+      <Surface className="group w-full h-22 px-3 py-2" hover>
+        <div className="flex justify-between text-center">
+          <h3 className="font-semibold">{`✧ ${username}'s entry · ${formattedDate}`}</h3>
+          <DeleteEntryButton entryId={entry.id} />
+        </div>
+      </Surface>
+    </Link>
   );
 }
