77930f08a4853d200fcd0692c2f75c1de4131cfb
Dan G <daniel.w.gray1994@gmail.com>
2026-02-15 14:47:08 -0500

    styled EntryCard further and added trash can button

--- src/components/entries/journal/EntryCard.tsx
+++ src/components/entries/journal/EntryCard.tsx
@@ -1,6 +1,7 @@
 import { EntryDTO } from "@/src/lib/entries/types";
 import Surface from "../../ui/Surface";
 import { formatEntryDate } from "@/src/lib/utils/time";
+import { TrashIcon } from "lucide-react";
 
 type EntryCardProps = {
   username: string;
@@ -11,10 +12,13 @@ export default function EntryCard({ username, entry }: EntryCardProps) {
   const formattedDate = formatEntryDate(entry.createdAt);
 
   return (
-    <Surface className="w-full h-22 px-3 py-2" hover>
-      <li>
-        <h3>{`${username}'s Entry · ${formattedDate}`}</h3>
-      </li>
-    </Surface>
+    <li>
+      <Surface className="w-full h-22 px-3 py-2" hover>
+        <div className="flex justify-between text-center">
+          <h3 className="font-semibold">{`✧ ${username}'s entry · ${formattedDate}`}</h3>
+          <TrashIcon className="h-4 w-4 text-red-400/75 hover:text-red-500/75 duration-200" />
+        </div>
+      </Surface>
+    </li>
   );
 }
